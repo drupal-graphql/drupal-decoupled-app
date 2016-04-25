804501112b95c2e6fe51bf326123ed79256b9bab
Sebastian Siemssen <fubhy@fubhy.com>
2016-04-25 14:43:33 +0200

    Fixing path resolving in server.prod.js.

--- server/server.prod.js
+++ server/server.prod.js
@@ -1,13 +1,13 @@
 const path = require('path');
+const buildPath = path.resolve(process.cwd(), 'build');
 const express = require('express');
-const options = require('../internals/webpack/webpack.prod');
-const render = require(path.resolve(process.cwd(), 'build', 'server')).default;
+const render = require(buildPath + '/server').default;
 
 module.exports = (app) => {
-  app.set('views', path.resolve(process.cwd(), 'build', 'views'));
+  app.set('views', buildPath + '/views');
   app.set('view engine', 'ejs');
 
-  app.use(options.output.publicPath, express.static(options.output.path));
+  app.use('/', express.static(buildPath + '/public'));
 
   // This is where the server-side rendering magic happens.
   app.get('*', render);
