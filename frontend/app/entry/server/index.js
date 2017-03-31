cdbcd8474366168cb57aa14adcae86a841881c65
Sebastian Siemssen <sebastian@amazeelabs.com>
2017-03-31 19:29:32 +0200

    Upgrading react-scripts and polyfilling fetch on the server side manually.

--- frontend/app/entry/server/index.js
+++ frontend/app/entry/server/index.js
@@ -1,23 +1,23 @@
 // @flow
 
+// Fetch polyfill for node.
+import 'fetch-everywhere/fetch-npm-node';
+
 import path from 'path';
 import express from 'express';
 import compression from 'compression';
 import { renderFile } from 'ejs';
 
 // Express middleware for server side rendering.
-const rendererMiddleware = (env) => (
-  req: Object,
-  res: Object,
-  next: Function,
-): void => {
-  // Do not use this middleware for anything that looks like a static file.
-  if (req.url.match(/\./)) {
-    next();
-  } else {
-    require('./render').default(env, req, res, next); // eslint-disable-line global-require
-  }
-};
+const rendererMiddleware = env =>
+  (req: Object, res: Object, next: Function): void => {
+    // Do not use this middleware for anything that looks like a static file.
+    if (req.url.match(/\./)) {
+      next();
+    } else {
+      require('./render').default(env, req, res, next); // eslint-disable-line global-require
+    }
+  };
 
 const createServer = (paths: { [key: string]: string }, env: Object) => {
   const port = env.PORT || 3000;
@@ -29,10 +29,12 @@ const createServer = (paths: { [key: string]: string }, env: Object) => {
   app.set('view engine', 'ejs');
   app.engine('ejs', renderFile);
 
-  app.use(express.static(path.join(paths.appBuild, 'public'), {
-    // Cache static files for a year.
-    maxAge: 31536000000,
-  }));
+  app.use(
+    express.static(path.join(paths.appBuild, 'public'), {
+      // Cache static files for a year.
+      maxAge: 31536000000,
+    }),
+  );
 
   // Use the server side rendering middleware..
   app.use(rendererMiddleware(env));
