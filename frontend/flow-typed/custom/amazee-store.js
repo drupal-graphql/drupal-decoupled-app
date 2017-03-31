800663c1bfa27d4a6e3ee8655dafe7a10194b943
Sebastian Siemssen <sebastian@amazeelabs.com>
2017-03-31 18:06:10 +0200

    Upgrading stack.

--- frontend/flow-typed/custom/amazee-store.js
+++
@@ -1,6 +0,0 @@
-import type { Store } from 'redux';
-
-declare type AmazeeStore<S, A> = Store<S, A> & {
-  asyncReducers: Object,
-  apolloClient: any,
-};
--- frontend/flow-typed/custom/globals.js
+++
@@ -1,5 +0,0 @@
-declare var __CLIENT__: boolean;
-declare var __SERVER__: boolean;
-declare var __DEVELOPMENT__: boolean;
-declare var __PRODUCTION__: boolean;
-declare var System : Object;
