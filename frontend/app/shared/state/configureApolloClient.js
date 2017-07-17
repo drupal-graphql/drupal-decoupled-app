c92f91af37c5a5a4e39cc9483aa8876d16d3a2c2
Sebastian Siemssen <fubhy@fubhy.com>
2017-07-17 11:46:20 +0200

    Upgrading graphql module.

--- frontend/app/shared/state/configureApolloClient.js
+++ frontend/app/shared/state/configureApolloClient.js
@@ -123,7 +123,7 @@ const configureApolloClient = (
   queryMap: Object,
   introspectionData: Object,
 ) => {
-  const isProduction = __PRODUCTION__ || true;
+  const isProduction = __PRODUCTION__;
   const hasApiVersion = !!apiVersion;
 
   // Use xdebug in development.
